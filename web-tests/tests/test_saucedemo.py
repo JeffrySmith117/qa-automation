994ccb5d3993a009c968d8a88c12adf1e9955f89
JeffrySmith117 <smithjs9225@gmail.com>
2026-05-10 23:49:27 -0300

    Fix complete SauceDemo e2e flow

--- web-tests/tests/test_saucedemo.py
+++ web-tests/tests/test_saucedemo.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_full_purchase_e2e(driver):
     wait = WebDriverWait(driver, 20)
 
+    # abre site
     driver.get("https://www.saucedemo.com/")
 
     # login
@@ -13,45 +14,99 @@ def test_full_purchase_e2e(driver):
         EC.visibility_of_element_located((By.ID, "user-name"))
     ).send_keys("standard_user")
 
-    driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.find_element(By.ID, "login-button").click()
+    driver.find_element(
+        By.ID,
+        "password"
+    ).send_keys("secret_sauce")
+
+    login_button = wait.until(
+        EC.presence_of_element_located((By.ID, "login-button"))
+    )
+
+    driver.execute_script(
+        "arguments[0].click();",
+        login_button
+    )
 
     # adiciona produto
-    wait.until(
-        EC.element_to_be_clickable(
+    add_button = wait.until(
+        EC.presence_of_element_located(
             (By.ID, "add-to-cart-sauce-labs-backpack")
         )
-    ).click()
+    )
 
-    # carrinho
-    wait.until(
-        EC.element_to_be_clickable(
+    driver.execute_script(
+        "arguments[0].click();",
+        add_button
+    )
+
+    # abre carrinho
+    cart_button = wait.until(
+        EC.presence_of_element_located(
             (By.CLASS_NAME, "shopping_cart_link")
         )
-    ).click()
+    )
+
+    driver.execute_script(
+        "arguments[0].click();",
+        cart_button
+    )
 
     # checkout
+    checkout_button = wait.until(
+        EC.presence_of_element_located((By.ID, "checkout"))
+    )
+
+    driver.execute_script(
+        "arguments[0].click();",
+        checkout_button
+    )
+
+    # espera página checkout carregar
     wait.until(
-        EC.element_to_be_clickable((By.ID, "checkout"))
-    ).click()
+        EC.url_contains("checkout-step-one")
+    )
 
     # formulário
-    wait.until(
+    first_name = wait.until(
         EC.visibility_of_element_located((By.ID, "first-name"))
-    ).send_keys("QA")
+    )
 
-    driver.find_element(By.ID, "last-name").send_keys("Tester")
-    driver.find_element(By.ID, "postal-code").send_keys("12345")
+    first_name.clear()
+    first_name.send_keys("QA")
+
+    last_name = driver.find_element(By.ID, "last-name")
+    last_name.clear()
+    last_name.send_keys("Tester")
+
+    postal_code = driver.find_element(By.ID, "postal-code")
+    postal_code.clear()
+    postal_code.send_keys("12345")
 
     # continue
+    continue_button = wait.until(
+        EC.presence_of_element_located((By.ID, "continue"))
+    )
+
+    driver.execute_script(
+        "arguments[0].click();",
+        continue_button
+    )
+
+    # espera próxima etapa
     wait.until(
-        EC.element_to_be_clickable((By.ID, "continue"))
-    ).click()
+        EC.url_contains("checkout-step-two")
+    )
 
     # finish
-    wait.until(
-        EC.element_to_be_clickable((By.ID, "finish"))
-    ).click()
+    finish_button = wait.until(
+        EC.presence_of_element_located((By.ID, "finish"))
+    )
+
+    driver.execute_script(
+        "arguments[0].click();",
+        finish_button
+    )
 
     # valida sucesso
     success_message = wait.until(
