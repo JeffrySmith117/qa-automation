02c7983d2c95b5ec397e312fb9af164fdfabee36
JeffrySmith117 <smithjs9225@gmail.com>
2026-05-10 23:36:53 -0300

    Fix checkout buttons with JS click

--- web-tests/tests/test_saucedemo.py
+++ web-tests/tests/test_saucedemo.py
@@ -6,40 +6,32 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_full_purchase_e2e(driver):
     wait = WebDriverWait(driver, 20)
 
-    # abre site
     driver.get("https://www.saucedemo.com/")
 
-    # login
     wait.until(
         EC.visibility_of_element_located((By.ID, "user-name"))
     ).send_keys("standard_user")
 
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
 
-    wait.until(
-        EC.element_to_be_clickable((By.ID, "login-button"))
-    ).click()
+    driver.find_element(By.ID, "login-button").click()
 
-    # adiciona produto
     wait.until(
         EC.element_to_be_clickable(
             (By.ID, "add-to-cart-sauce-labs-backpack")
         )
     ).click()
 
-    # abre carrinho
     wait.until(
         EC.element_to_be_clickable(
             (By.CLASS_NAME, "shopping_cart_link")
         )
     ).click()
 
-    # checkout
     wait.until(
         EC.element_to_be_clickable((By.ID, "checkout"))
     ).click()
 
-    # espera formulário aparecer
     wait.until(
         EC.visibility_of_element_located((By.ID, "first-name"))
     ).send_keys("QA")
@@ -47,17 +39,18 @@ def test_full_purchase_e2e(driver):
     driver.find_element(By.ID, "last-name").send_keys("Tester")
     driver.find_element(By.ID, "postal-code").send_keys("12345")
 
-    # continua
-    wait.until(
-        EC.element_to_be_clickable((By.ID, "continue"))
-    ).click()
+    continue_button = wait.until(
+        EC.presence_of_element_located((By.ID, "continue"))
+    )
 
-    # finish
-    wait.until(
-        EC.element_to_be_clickable((By.ID, "finish"))
-    ).click()
+    driver.execute_script("arguments[0].click();", continue_button)
 
-    # valida mensagem
+    finish_button = wait.until(
+        EC.presence_of_element_located((By.ID, "finish"))
+    )
+
+    driver.execute_script("arguments[0].click();", finish_button)
+
     success_message = wait.until(
         EC.visibility_of_element_located(
             (By.CLASS_NAME, "complete-header")
