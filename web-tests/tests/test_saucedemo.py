4b52d5d5cd6e83465aca0d84519d2f980a03fb01
JeffrySmith117 <smithjs9225@gmail.com>
2026-05-10 23:45:50 -0300

    Simplify SauceDemo flow

--- web-tests/tests/test_saucedemo.py
+++ web-tests/tests/test_saucedemo.py
@@ -3,71 +3,57 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def fill_input(driver, element_id, value):
-    element = driver.find_element(By.ID, element_id)
-
-    driver.execute_script("""
-        arguments[0].value = arguments[1];
-        arguments[0].dispatchEvent(new Event('input', { bubbles: true }));
-        arguments[0].dispatchEvent(new Event('change', { bubbles: true }));
-    """, element, value)
-
-
 def test_full_purchase_e2e(driver):
     wait = WebDriverWait(driver, 20)
 
     driver.get("https://www.saucedemo.com/")
 
+    # login
     wait.until(
         EC.visibility_of_element_located((By.ID, "user-name"))
     ).send_keys("standard_user")
 
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
-
     driver.find_element(By.ID, "login-button").click()
 
+    # adiciona produto
     wait.until(
         EC.element_to_be_clickable(
             (By.ID, "add-to-cart-sauce-labs-backpack")
         )
     ).click()
 
+    # carrinho
     wait.until(
         EC.element_to_be_clickable(
             (By.CLASS_NAME, "shopping_cart_link")
         )
     ).click()
 
+    # checkout
     wait.until(
         EC.element_to_be_clickable((By.ID, "checkout"))
     ).click()
 
+    # formulário
     wait.until(
         EC.visibility_of_element_located((By.ID, "first-name"))
-    )
+    ).send_keys("QA")
 
-    fill_input(driver, "first-name", "QA")
-    fill_input(driver, "last-name", "Tester")
-    fill_input(driver, "postal-code", "12345")
+    driver.find_element(By.ID, "last-name").send_keys("Tester")
+    driver.find_element(By.ID, "postal-code").send_keys("12345")
 
-    continue_button = driver.find_element(By.ID, "continue")
-
-    driver.execute_script(
-        "arguments[0].click();",
-        continue_button
-    )
-
+    # continue
     wait.until(
-        EC.visibility_of_element_located((By.ID, "finish"))
-    )
+        EC.element_to_be_clickable((By.ID, "continue"))
+    ).click()
 
-    finish_button = driver.find_element(By.ID, "finish")
+    # finish
+    wait.until(
+        EC.element_to_be_clickable((By.ID, "finish"))
+    ).click()
 
-    driver.execute_script(
-        "arguments[0].click();",
-        finish_button
-    )
-
+    # valida sucesso
     success_message = wait.until(
         EC.visibility_of_element_located(
             (By.CLASS_NAME, "complete-header")
