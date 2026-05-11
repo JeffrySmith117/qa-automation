c904ca3c5973d03fb32535b9774340886c5c004d
JeffrySmith117 <smithjs9225@gmail.com>
2026-05-10 22:45:31 -0300

    fix: revert cart_page to simple version

--- web-tests/pages/cart_page.py
+++ web-tests/pages/cart_page.py
@@ -1,6 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 
@@ -9,9 +7,6 @@ class CartPage(BasePage):
     _CHECKOUT_BTN = (By.ID, "checkout")
 
     def get_item_count(self):
-        WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "cart_contents"))
-        )
         return len(self.driver.find_elements(*self._ITEMS))
 
     def proceed_to_checkout(self):
