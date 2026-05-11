b195a951c34d07c11b2c46dc152e98c77939ed16
JeffrySmith117 <smithjs9225@gmail.com>
2026-05-10 22:24:35 -0300

    fix: add explicit wait after each product added to cart

--- web-tests/pages/inventory_page.py
+++ web-tests/pages/inventory_page.py
@@ -1,26 +1,31 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 
 class InventoryPage(BasePage):
     _TITLE = (By.CLASS_NAME, "title")
-    _ADD_TO_CART_BTN = (By.CSS_SELECTOR, "[data-test^='add-to-cart']")
     _CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
     _CART_ICON = (By.CLASS_NAME, "shopping_cart_link")
 
     def is_loaded(self):
         return self.get_text(self._TITLE) == "Products"
 
-    def add_first_product(self):
-        """Clicks the first available 'Add to cart' button."""
-        buttons = self.driver.find_elements(*self._ADD_TO_CART_BTN)
-        buttons[0].click()
-
     def add_products(self, count=2):
-        """Adds N products to the cart."""
-        buttons = self.driver.find_elements(*self._ADD_TO_CART_BTN)
-        for btn in buttons[:count]:
-            btn.click()
+        """Adiciona N produtos ao carrinho um por vez com espera entre cada um."""
+        added = 0
+        while added < count:
+            buttons = self.driver.find_elements(
+                By.CSS_SELECTOR, "[data-test^='add-to-cart']"
+            )
+            if added < len(buttons):
+                buttons[added].click()
+                # Espera o badge atualizar antes de continuar
+                WebDriverWait(self.driver, 5).until(
+                    lambda d: self.get_cart_count() == added + 1
+                )
+                added += 1
 
     def get_cart_count(self):
         badges = self.driver.find_elements(*self._CART_BADGE)
