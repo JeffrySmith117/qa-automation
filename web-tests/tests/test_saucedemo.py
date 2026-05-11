bc81d098a3fa3d3d6118473a963feed036d7bdf4
JeffrySmith117 <smithjs9225@gmail.com>
2026-05-10 23:42:24 -0300

    Fix SauceDemo checkout flow

--- web-tests/tests/test_saucedemo.py
+++ web-tests/tests/test_saucedemo.py
@@ -3,6 +3,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+def fill_input(driver, element_id, value):
+    element = driver.find_element(By.ID, element_id)
+
+    driver.execute_script("""
+        arguments[0].value = arguments[1];
+        arguments[0].dispatchEvent(new Event('input', { bubbles: true }));
+        arguments[0].dispatchEvent(new Event('change', { bubbles: true }));
+    """, element, value)
+
+
 def test_full_purchase_e2e(driver):
     wait = WebDriverWait(driver, 20)
 
@@ -34,22 +44,29 @@ def test_full_purchase_e2e(driver):
 
     wait.until(
         EC.visibility_of_element_located((By.ID, "first-name"))
-    ).send_keys("QA")
-
-    driver.find_element(By.ID, "last-name").send_keys("Tester")
-    driver.find_element(By.ID, "postal-code").send_keys("12345")
-
-    continue_button = wait.until(
-        EC.presence_of_element_located((By.ID, "continue"))
     )
 
-    driver.execute_script("arguments[0].click();", continue_button)
+    fill_input(driver, "first-name", "QA")
+    fill_input(driver, "last-name", "Tester")
+    fill_input(driver, "postal-code", "12345")
 
-    finish_button = wait.until(
-        EC.presence_of_element_located((By.ID, "finish"))
+    continue_button = driver.find_element(By.ID, "continue")
+
+    driver.execute_script(
+        "arguments[0].click();",
+        continue_button
     )
 
-    driver.execute_script("arguments[0].click();", finish_button)
+    wait.until(
+        EC.visibility_of_element_located((By.ID, "finish"))
+    )
+
+    finish_button = driver.find_element(By.ID, "finish")
+
+    driver.execute_script(
+        "arguments[0].click();",
+        finish_button
+    )
 
     success_message = wait.until(
         EC.visibility_of_element_located(
